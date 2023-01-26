46e8423f4e211031071d2d080d64e75c31132528
Jesse Arzate <97304170+baileyarzate@users.noreply.github.com>
2023-01-26 11:41:31 -0800

    Add files via upload

--- bonftolint.py
+++ bonftolint.py
@@ -90,14 +90,8 @@ def Kfactor(n, f = None, alpha = 0.05, P = 0.99, side = 1, method = 'HE', m=50):
                 K = opt.minimize(fun=Fun3, x0=k2, args=(P,n,f,alpha,m,delta), method = 'L-BFGS-B')['x']
                 return float(K)
             elif method == 'EXACT':
-                print('This method prodcues slightly different results',
-                      'when compared to R. Take these results with',
-                      'a grain of salt. The range of error',
-                      'is between approximately (1e-3,2.0).',
-                      'If this method is abosolutely needed',
-                      'use R instead.')
                 def fun1(z,df1,P,X,n):
-                    k = (scipy.stats.chi2.sf(df1*scipy.stats.chi2.ppf(P,1,z**2)/X**2,df=df1)*np.exp(-0.5*n*z**2))
+                    k = (scipy.stats.chi2.cdf(df1*scipy.stats.ncx2.ppf(P,1,z**2)/X**2,df=df1)*np.exp(-0.5*n*z**2))
                     return k
                 def fun2(X,df1,P,n,alpha,m):
                     return integrate.quad(fun1,a =0, b = 5, args=(df1,P,X,n),limit=m)
@@ -333,3 +327,5 @@ Examples
     else:
         s = '\nThe inputted function returns a list, if this is ever happens email me at baileyarzate@gmail.com so I can add code here to handle it. I couldn\'t find a case while testing'
         return s
+    
+    
